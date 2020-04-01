08a8ff84deed465730d8cc67bd5c289ed567e4ad
Simon Tye <simontye@uark.edu>
2020-04-01 16:51:54 -0500

    binned habitat types: remix

--- sandbox/20.03.30-InitialExploration.R
+++ sandbox/20.03.30-InitialExploration.R
@@ -1,17 +1,30 @@
-setwd("~/2020_Zygoptera/")
+###############################################################
+# Convergence of odonate body colors and patterns across habitats
+# 2020.04.01
+# JB, ST
+###############################################################
+
+rm(list=ls())
+
+# setwd("~/2020_Zygoptera/")
+setwd("/Users/simontye/Documents/Research/Projects/Zygoptera/2020_Zygoptera")
 
 require(ape)
 require(corHMM)
 
 # three different tree files all with the same tips. Dunno what the difference is
 t <- read.nexus("data/trees/tree.nex")
-#phy <- read.nexus("data/trees/Anisoptera_tree.nex")
-#phy <- read.nexus("data/trees/Zygoptera_tree.nex")
+#phy <- read.nexus("data/trees/Anisoptera_tree.nex") # dragons, but not
+#phy <- read.nexus("data/trees/Zygoptera_tree.nex") # damsels, not not
 d <- read.csv("data/opdb.csv")
 
+# bin body colors and pattern types
 black <- ifelse(d$body_colors == "black", 1, 0)
 plain <- ifelse(d$body_patterns == "plain", 1, 0)
-ephemeral <- ifelse(d$aquatic_habitats == "ephemeral", 1, 0)
+ephemeral <- ifelse(d$aquatic_habitats == "ephemeral", 1,
+                    ifelse(d$aquatic_habitats == "wetland", 1,
+                           ifelse(d$aquatic_habitats == "lake", 0,
+                                  ifelse(d$aquatic_habitats == "pond", 0, 2))))
 
 # create a dataset
 dat <- data.frame(
@@ -21,11 +34,14 @@ dat <- data.frame(
   ephemeral = ephemeral
 )
 
+# Remove pond and stream habitat types
+dat <- subset(dat, ephemeral!=2)
+
 # make sure phy and dataset are working with the same set of tips
 phy <- drop.tip(t, t$tip.label[!t$tip.label %in% dat$sp])
 dat <- dat[as.character(dat$sp) %in% phy$tip.label,]
 
-# there are many doubles in the dataset. find the polymorphism and create a new state.
+# there are many doubles in the dataset. find the polymorphism and create a new state
 PolyDat <- data.frame(sp = unique(dat$sp), black = 0, plain = 0, ephemeral = 0)
 for(sp in unique(dat$sp)){
   tmpDat <- dat[dat$sp %in% sp,]
